87d02940c548fcaba3428030f2fd82d326fd9e92
Jobin Thavu Varghese <jobinthavuvarghese@Jobins-MacBook-Pro.local>
2022-05-31 21:13:44 +0530

    build commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      <footer>
-        ReceiptMail v2.0.
+    <div className='divStyle'>
+      <footer className='centerform'>
+        ReceiptMail v2.0
       </footer>
     </div>
   )
