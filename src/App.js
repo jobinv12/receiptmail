7de271a5b116be7d5bb7e2e251287e1c437ba810
Jobin Thavu Varghese <jobinthavuvarghese@Jobins-MacBook-Pro.local>
2022-06-01 10:33:43 +0530

    deleted footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Footer from './Components/Footer';
 import ReceiptForm from './Components/ReceiptForm';
 
 function App() {
   return (
     <div>
       <ReceiptForm/>
-      <Footer></Footer>
     </div>
   );
 }
